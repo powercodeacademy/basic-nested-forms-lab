b6c697cf431653aedff462ae3de3a314fd072713
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-21 16:11:00 -0400

    Accept nested ingredient attributes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: '0')
   end
 
   def create
+    Recipe.create(recipe_params)
+  end
+
+  private 
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
