46a4db2892f84b5b3110cc23b068fce433f002e7
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-26 12:36:46 -0400

    Complete lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,36 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+      :name,
+      :quantity
+    ])
   end
 end
+
+# {
+#   :recipe => {
+#     :title => "Chocolate Cake",
+#     :ingredients_attributes => {
+#       "0" => {
+#         :name => "sugar",
+#         :quantity => "1 cup"
+#       },
+#       "1" => {
+#         :name => "vanilla",
+#         :quantity => "1 tablespoon"
+#       }
+#     }
+#   }
+# }
